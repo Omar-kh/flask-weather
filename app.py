810002c5818403f5b3242ab1dc02e0bbc6b20f2a
Omar <omar.kadiri.hassani@gmail.com>
2019-07-19 12:45:50 +0200

    add display city

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ def index():
     r = py_request.get(
         f"http://api.ipstack.com/{user_ip}?access_key=ac818c67553e5e90adad3de5a376ef16")
     user_info = r.json()
-    lat = r.json().get('latitude', 'Not found')
-    lon = r.json().get('longitude', 'Not found')
+    city = user_info.get("city", 'not found')
+    lat = user_info.get('latitude', 'Not found')
+    lon = user_info.get('longitude', 'Not found')
     print(lat, lon)
-    return user_info
+    return render_template("index.html", user_ip=user_ip, city=city)
 
 
 if __name__ == "__main__":
