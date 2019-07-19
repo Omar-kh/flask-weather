a34d8325b802afe9a9d7c78b49300ed085c44bd5
Omar <omar.kadiri.hassani@gmail.com>
2019-07-19 12:10:25 +0200

    modify main response

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def index():
     user_ip = request.remote_addr
     r = py_request.get(
         f"http://api.ipstack.com/{user_ip}?access_key=ac818c67553e5e90adad3de5a376ef16")
-    print(r.json())
+    user_info = r.json()
     lat = r.json().get('latitude', 'Not found')
     lon = r.json().get('longitude', 'Not found')
     print(lat, lon)
-    return render_template('index.html', user_ip=user_ip)
+    return user_info
 
 
 if __name__ == "__main__":
